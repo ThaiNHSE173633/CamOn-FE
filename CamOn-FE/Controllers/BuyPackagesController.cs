e8ece2a501cbdfb134313d5ab1e2a06a95bd09b7
ThaiNHSE173633 <thainhse173633@fpt.edu.vn>
2024-07-11 21:03:33 +0700

    added package details

--- CamOn-FE/Controllers/BuyPackagesController.cs
+++ CamOn-FE/Controllers/BuyPackagesController.cs
@@ -6,15 +6,18 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using BusinessObjects;
+using Microsoft.AspNetCore.Identity;
 
 namespace CamOn_FE.Controllers
 {
     public class BuyPackagesController : Controller
     {
         private readonly AppDbContext _context;
+        private readonly UserManager<Account> _userManager;
 
-        public BuyPackagesController(AppDbContext context)
+        public BuyPackagesController(UserManager<Account> userManager, AppDbContext context)
         {
+            _userManager = userManager; 
             _context = context;
         }
 
@@ -25,5 +28,28 @@ namespace CamOn_FE.Controllers
                           View(await _context.Packages.ToListAsync()) :
                           Problem("Entity set 'AppDbContext.Packages'  is null.");
         }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Packages == null)
+            {
+                return NotFound();
+            }
+
+            var package = await _context.Packages
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (package == null)
+            {
+                return NotFound();
+            }
+            var currentUser = await _userManager.GetUserAsync(User);
+            if (currentUser == null)
+            {
+                return RedirectToAction("Index","Home");
+            }
+            ViewData["userEmail"] = currentUser.Email;
+
+            return View(package);
+        }
+
     }
 }
